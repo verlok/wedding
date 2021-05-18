e9c847fd8a77b39dbcbb6d4b7ac23b76d9c0c805
verlok <andrea.verlicchi@gmail.com>
2021-05-18 22:18:29 +0200

    Text button changing + sposi webp

--- page.js
+++ page.js
@@ -1,8 +1,8 @@
-const toggleNerdMode = () => {
+const toggleLayout = () => {
   const styles = document.getElementsByClassName("cssSwitcher");
   for (let style of styles) {
     style.media = style.media === "screen" ? "offscreen" : "screen";
   }
 };
-const button = document.getElementById("toggleNerdMode");
-button.addEventListener("click", toggleNerdMode);
+const button = document.getElementById("toggleLayout");
+button.addEventListener("click", toggleLayout);
